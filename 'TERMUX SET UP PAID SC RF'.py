e5e25e711cf535097bd98262bd38272135b5cbf0
Cyber Rabbi <rabbicyber160@gmail.com>
2024-10-03 07:26:29 +0600

    Add files via upload

---
+++ 'TERMUX SET UP PAID SC RF'.py
@@ -0,0 +1,112 @@
+from os import system as RABBI
+import sys
+import time
+RABBI('xdg-open https://facebook.com/groups/1000743501516075/')
+
+def line():
+    print("_________________________________________")
+
+baner = """
+\033[1;32m
+       ██████   █████  ██████  ██████  ██ 
+       ██   ██ ██   ██ ██   ██ ██   ██ ██ 
+       ██████  ███████ ██████  ██████  ██ 
+       ██   ██ ██   ██ ██   ██ ██   ██ ██ 
+       ██   ██ ██   ██ ██████  ██████  ██
+\033[1;36m
+
+ \033[1;91m┏━━━━━━━━━━━\033[1;93m┃━A━┃━B━┃\033[1;91m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+ \033[1;91m┃ \033[1;92m┏━━━━━━━━━\033[1;91m┃━C━┃━D━┃\033[1;92m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓ \033[1;91m1┃
+ \033[1;91m┃ \033[1;92m┃ \033[1;93m┏━━━━━━━\033[1;92m┃━E━┃━F━┃\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━┓ \033[1;92mH┃ \033[1;91m2┃
+ \033[1;91m┃ \033[1;92m┃ \033[1;93m┃ \033[1;95m[✓] AUTHOR   \033[1;93m❯\033[1;92m❯\033[1;91m❯ \033[1;92mCYBER RABBI RF          \033[1;93mA┃ \033[1;92mI┃ \033[1;91m3┃
+ \033[1;91m┃ \033[1;92m┃ \033[1;93m┃ \033[1;95m[✓] TOOL     \033[1;93m❯\033[1;92m❯\033[1;91m❯ \033[1;92mTERMUX PAID SET UP           \033[1;93mB┃ \033[1;92mJ┃ \033[1;91m4┃
+ \033[1;91m┃ \033[1;92m┃ \033[1;93m┃ \033[1;95m[✓] TOOL PAID \033[1;93m❯\033[1;92m❯\033[1;91m❯ \033[1;92m04                     \033[1;93mC┃ \033[1;92mK┃ \033[1;91m5┃
+ \033[1;91m┃ \033[1;92m┃ \033[1;93m┃ \033[1;95m[✓] STATUS   \033[1;93m❯\033[1;92m❯\033[1;91m❯ \033[1;92mPAID                   \033[1;93mD┃ \033[1;92mK┃ \033[1;91m6┃
+ \033[1;91m┃ \033[1;92m┃ \033[1;93m┃ \033[1;95m[✓] VERSION  \033[1;93m❯\033[1;92m❯\033[1;91m❯ \033[1;92m4.4                    \033[1;93mE┃ \033[1;92mK┃ \033[1;91m7┃
+ \033[1;91m┃ \033[1;92m┃ \033[1;93m┃ \033[1;95m[✓] SYSTEM   \033[1;93m❯\033[1;92m❯\033[1;91m❯ \033[1;92mDATA & WIFI            \033[1;93mF┃ \033[1;92mL┃ \033[1;91m8┃
+ \033[1;91m┃ \033[1;92m┃ \033[1;93m┃ \033[1;95m[✓] NETWORK  \033[1;93m❯\033[1;92m❯\033[1;91m❯ \033[1;92m3G - 4G -5G            \033[1;93mG┃ \033[1;92mM┃ \033[1;91m9┃
+ \033[1;91m┃ \033[1;92m┃ \033[1;93m┃ \033[1;95m[✓] FACEBOOK \033[1;93m❯\033[1;92m❯\033[1;91m❯ \033[1;92mদু্ঁষ্টু্ঁ ব্ঁউ্ঁয়ে্ঁর্ঁ মি্ঁষ্টি্ঁ ব্ঁর্ঁ     \033[1;93mH┃ \033[1;92mN┃ \033[1;91mA┃
+ \033[1;91m┃ \033[1;92m┃ \033[1;93m┃ \033[1;95m[✓] GITHUB   \033[1;93m❯\033[1;92m❯\033[1;91m❯ \033[1;92mcyber rabbi-404         \033[1;93mI┃ \033[1;92mP┃ \033[1;91mB┃
+ \033[1;91m┃ \033[1;92m┃ \033[1;93m┃ \033[1;95m[✓] WHATSAPP \033[1;93m❯\033[1;92m❯\033[1;91m❯ \033[1;92m+880         \033[1;93mJ┃ \033[1;92mQ┃ \033[1;91mC┃
+ \033[1;91m┃ \033[1;92m┃ \033[1;93m┗━━━━━━━━━━━━━━━━━━━━━━━━━━\033[1;92m┃━1━┃━2━┃\033[1;93m━━━━━━━┛ \033[1;92mR┃ \033[1;91mD┃
+ \033[1;91m┃ \033[1;92m┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[1;91m┃━3━┃━4━┃\033[1;92m━━━━━━━━━━┛ \033[1;91mE┃
+ \033[1;91m┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[1;93m┃━5━┃━6━┃\033[1;91m━━━━━━━━━━━━━┛
+"""
+def clear():
+    RABBI('clear')
+    print(baner)
+    line()
+    print('\x1b[1;37m[\x1b[38;5;46m+\x1b[1;37m] AUTHOR   : RF CYBER TEAM  ')
+    print('\x1b[1;37m[\x1b[38;5;46m+\x1b[1;37m] GITHUB   : https://github.com/cyberrabbi404 ')
+    print('\x1b[1;37m[\x1b[38;5;46m+\x1b[1;37m] PROJECT  : TERMUX SETUP ')
+    print('\x1b[1;37m[\x1b[38;5;46m+\x1b[1;37m] FACEBOOK : CYBER RABBI RF  ')
+    line()
+
+
+def basic():
+    RABBI('apt update')
+    RABBI('apt upgrade')
+    RABBI('pkg install nano')
+    RABBI('gem install lolcat')
+    RABBI('pkg install ruby')
+    RABBI('pip install requests')
+    RABBI('pip install mechanize')
+    RABBI('pip install bs4')
+    RABBI('pip install features')
+    RABBI('pip install futures')
+    clear()
+    print('YOUR TERMUX IS READY TO USE ')
+
+
+def full():
+    RABBI('apt update')
+    RABBI('apt upgrade')
+    RABBI('pkg install nano')
+    RABBI('gem install lolcat')
+    RABBI('pkg install ruby')
+    RABBI('pip install requests')
+    RABBI('pip install mechanize')
+    RABBI('pip install bs4')
+    RABBI('pip install features')
+    RABBI('pip install futures')
+    RABBI('pip2 install mechanize')
+    RABBI('pip2 install requests')
+    RABBI('pip2 install bs4')
+    RABBI('pkg install python2')
+    RABBI('apt install tmux')
+    RABBI('pkg install curl')
+    RABBI('pkg install toilet')
+    RABBI('pkg install cowsay')
+    RABBI('pkg install figlet')
+    RABBI('pkg install awesomeshot')
+    RABBI('pkg install inotify-tools')
+    RABBI('pkg install clang')
+    RABBI('pkg install bat')
+    RABBI('pkg install imagemagick')
+    RABBI('pkg install exa')
+    RABBI('pkg install lf')
+    RABBI('pkg install mpd')
+    RABBI('pkg install neovim')
+    RABBI('pkg install openssh')
+    RABBI('pkg install neofetch')
+    RABBI('pkg install termux-api')
+    RABBI('pkg install tmux')
+    RABBI('pkg install zsh')
+    RABBI('pkg i -y git bc')
+    clear()
+    print('YOUR TERMUX IS READY TO USE')
+
+def main():
+	clear()
+	print("(1) TERMUX BASIC SETUP")
+	print("(2) TERMUX FULL SETUP")
+	print("(3) CONTACT ADMIN")
+	line()
+	rabbi=input("(??) CHOICE : ")
+	if rabbi in ['1','01']:
+		basic()
+	if rabbi in ['2','02']:
+		full()
+	else:
+		main()
+main()
